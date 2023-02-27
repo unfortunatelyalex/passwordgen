7d5c24477f302ec5909cd9c82e18c04eca0ce465
alex <rezeaxtxt@gmail.com>
2023-02-27 18:51:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -31,4 +31,4 @@ if __name__ == '__main__':
     if mode == 'dev':
         app.run(host='127.0.0.1', port=5000, debug=True)
     elif mode == "prod":
-        serve(app, host="127.0.0.1", port=5000)
+        serve(app, host="0.0.0.0", port=8000)
