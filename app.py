967c5092a5cfcb2a6d0d16d0514b0c1f7f3954af
alex <rezeaxtxt@gmail.com>
2023-03-02 21:07:27 +0100

    improved logging

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 import random
+import logging
+import traceback
+from time import strftime
 from waitress import serve
+from werkzeug.exceptions import HTTPException
+from logging.handlers import RotatingFileHandler
 from flask import Flask, render_template, request
 
+
+
+
+
 app = Flask(__name__)
+
+
 
 @app.route('/')
 def index():
@@ -24,6 +35,26 @@ def generate_password():
     password_groups = [password[i:i+5] for i in range(0, len(password), 5)]
     password_str = '-'.join(password_groups)
     return password_str
+    
+    
+
+@app.after_request
+def after_request(response):
+    timestamp = strftime('[%Y-%b-%d %H:%M]')
+    logger.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+    return response
+
+
+@app.errorhandler(HTTPException)
+def exceptions(e):
+    if isinstance(e, HTTPException):
+        tb = traceback.format_exc()
+        timestamp = strftime('[%Y-%b-%d %H:%M]')
+        logger.error('%s %s %s %s %s 5xx INTERNAL SERVER ERROR\n%s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, tb)
+        return e.status_code
+    return render_template(e=e), 500
+
+
 
 mode = 'prod'
 
@@ -31,4 +62,8 @@ if __name__ == '__main__':
     if mode == 'dev':
         app.run(host='127.0.0.1', port=8000, debug=True)
     elif mode == "prod":
-        serve(app, host="0.0.0.0", port=8000, url_scheme="https")
+        handler = RotatingFileHandler('app.log', maxBytes=100000, backupCount=3)
+        logger = logging.getLogger('tdm')
+        logger.setLevel(logging.ERROR)
+        logger.addHandler(handler)
+        serve(app, host="127.0.0.1", port=8000)
